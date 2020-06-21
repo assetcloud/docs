7db6b52d9b1cb0b9b68dd5e038579cd1752351d6
panzhaohui <panzhaohui@msn.com>
2020-06-21 12:44:30 +0800

    logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
 module.exports = {
     title: '云原生应用开发文档',
-    description: '本文档提供资产云应用中心上架应用相关技术文档',
+    description: '本文档提供资产云应用中心上架应用相关技术说明',
 
     themeConfig: {
+        logo: './ocia.png',
         nav: [
             { text: '首页', link: '/' },
             { text: '快速上手', link: '/Ues/' },
@@ -16,5 +17,7 @@ module.exports = {
             "/API explain/": ["", "one", "two"],
             "/Tools and resources/": ["", "one", "two"],
         }
+
+        
     }
 }
